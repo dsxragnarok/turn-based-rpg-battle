9d27383edbe3646332711bda345b84a6fe48a893
Kevin Phung <phung.kevin@gmail.com>
2016-09-15 05:33:02 -0600

    test: started a test file for action

---
+++ actionSpec.js
@@ -0,0 +1,15 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Action class', function () {
+   beforeEach(function () {
+
+   });
+
+   afterEach(function () {
+
+   });
+
+   it('should instantiate an action');
+   it('should have an perform method');
+});
