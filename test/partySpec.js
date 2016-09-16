26d867e92c8b3916425fe925b0c6236e39e4d2ff
Kevin Phung <phung.kevin@gmail.com>
2016-09-15 21:15:20 -0600

    chore: create party and its test files

---
+++ test/partySpec.js
@@ -0,0 +1,22 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Party class', function () {
+   var Party = require('../party');
+   var Unit = require('../unit');
+
+   beforeEach(function () {
+
+   });
+
+   afterEach(function () {
+
+   });
+
+   it('should instantiate a party');
+   it('should instantiate with zero members by default');
+   it('should be able to add a unit');
+   it('should be able to remove a unit');
+   it('should be able to find a unit it contains');
+   it('should know how many members it has');
+});
