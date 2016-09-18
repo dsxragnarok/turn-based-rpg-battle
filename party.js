b6af46ae147ebae8bf92ef53fd5be4ec5cd9ca5a
Kevin Phung <phung.kevin@gmail.com>
2016-09-17 22:44:13 -0600

    refactor: underscore to indicate private variable

--- party.js
+++ party.js
@@ -1,35 +1,35 @@
 var Unit = require('./unit');
 
 var Party = function () {
-   this.members = [];
+   this._members = [];
 
    if (arguments.length > 0 &&
       Array.isArray(arguments[0])) {
 
-      this.members = arguments[0];
+      this._members = arguments[0];
    }
 };
 
 Party.prototype = {
    size: function () {
-      return this.members.length;
+      return this._members.length;
    },
    add: function (unit) {
-      this.members.push(unit);
+      this._members.push(unit);
    },
    remove: function (name) {
       var index = this.findByName(name);
       var unit = null;
 
       if (index >= 0) {
-         unit = this.members.splice(index, 1)[0];
+         unit = this._members.splice(index, 1)[0];
       }
 
       return unit;
    },
    contains: function (unit) {
       if (unit instanceof Unit) {
-         return this.members.indexOf(unit) >= 0;
+         return this._members.indexOf(unit) >= 0;
       } else if (typeof unit === 'string') {
          return this.findByName(unit) >= 0;
       }
@@ -37,10 +37,10 @@ Party.prototype = {
       return false;
    },
    findByName: function (name) {
-      var i = 0, len = this.members.length;
+      var i = 0, len = this._members.length;
 
       for (i; i < len; i += 1) {
-         if (this.members[i].name === name) {
+         if (this._members[i].name === name) {
             return i;
          }
       }
